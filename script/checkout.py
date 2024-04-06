50bc3265560323b3cf19d54d7ff96eed1762d4a4
bytechan <81347393+bytechan@users.noreply.github.com>
2024-04-06 17:55:10 +0800

    use python2 instead

--- script/checkout.py
+++ script/checkout.py
@@ -41,13 +41,13 @@ def main():
   if 'windows' == common.host():
     env = os.environ.copy()
     env['PYTHONHTTPSVERIFY']='0'
-    subprocess.check_call(["python3", "tools/git-sync-deps"], env=env)
+    subprocess.check_call(["python", "tools/git-sync-deps"], env=env)
   else:
-    subprocess.check_call(["python3", "tools/git-sync-deps"])
+    subprocess.check_call(["python", "tools/git-sync-deps"])
 
   # fetch ninja
   print("> Fetching ninja")
-  subprocess.check_call(["python3", "bin/fetch-ninja"])
+  subprocess.check_call(["python", "bin/fetch-ninja"])
 
   return 0
 
