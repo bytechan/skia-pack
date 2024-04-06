d1f98cfd0392aa1cf8720f67616f9e12d7b9c9b3
bytechan <81347393+bytechan@users.noreply.github.com>
2024-04-06 15:42:01 +0800

    linux build

--- script/build.py
+++ script/build.py
@@ -73,8 +73,8 @@ def main():
     else:
         args += [
             'extra_cflags_cc=["-fno-exceptions", "-fno-rtti","-D_GLIBCXX_USE_CXX11_ABI=0"]',
-            'cc="gcc-9"',
-            'cxx="g++-9"',
+            'cc="gcc-5"',
+            'cxx="g++-5"',
         ]
   elif 'windows' == target:
     args += [
